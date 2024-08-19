e64fd21f6ce875b04178422eacba20312ea17b67
Rizky Daffy Al Fajr <162791897+RizkyDaffy@users.noreply.github.com>
2024-08-19 19:34:01 +0700

    Create besokList.tsx

---
+++ app/partials/besokList.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+export default function besokList() {
+  return (
+    <><div id="besok" className="tabcontent">
+        <main>
+            <div className="task">
+                <h3 >TUGAS BAHASA</h3>
+                <p>harus menjelaskan materi tentang ..</p>
+                <div className="deadline">
+                    <ion-icon name="time"></ion-icon>
+                    <span>  Deadline: Besok pukul 8:15</span>
+                </div>
+            </div>
+        </main>
+    </div></>
+  );
+}
