8512d91054f2ea35e4696ab45b9a9a0bc602686f
Rizky Daffy Al Fajr <162791897+RizkyDaffy@users.noreply.github.com>
2024-08-19 19:33:43 +0700

    Create bottomSection.tsx

---
+++ app/partials/bottomSection.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Link from 'next/link'
+
+export default function bottomSection() {
+  return (
+     <> <footer>
+          <Link className="linknavg" href="/"> <div className="nav-icon">
+              <ion-icon name="home"></ion-icon>
+           </div></Link>
+           <Link className="linknavg nav-icon" href="/buat">
+               <ion-icon name="create"></ion-icon>
+           </Link>
+        
+       </footer>
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script><script noModule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script></>
+  );
+}
