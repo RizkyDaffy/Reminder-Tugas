edfa52e819f9d74b3adff0a42e08cce0a78ad829
Rizky Daffy Al Fajr <162791897+RizkyDaffy@users.noreply.github.com>
2024-08-19 19:35:09 +0700

    Create profileHeader.tsx

---
+++ app/partials/profileHeader.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function profileHeader() {
+  return (
+    <><header>
+        <div className="profile">
+            <ion-icon name="person-circle" class="profile-icon"></ion-icon>
+            <h2>Rizky Ganteng</h2>
+        </div>
+    </header></>
+  );
+}
