5c43f33bbdb87193a9c3363c69786e467d1a0b7e
Rizky Daffy Al Fajr <162791897+RizkyDaffy@users.noreply.github.com>
2024-08-19 20:00:13 +0700

    Update besokList.tsx

--- app/partials/besokList.tsx
+++ app/partials/besokList.tsx
@@ -9,11 +9,12 @@ export default function besokList() {
                 <h3 >TUGAS BAHASA</h3>
                 <p>harus menjelaskan materi tentang ..</p>
                 <div className="deadline">
-                    
+                    <ion-icon name="time"></ion-icon>
                     <span>  Deadline: Besok pukul 8:15</span>
                 </div>
             </div>
         </main>
-    </div></>
+    </div>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script><script noModule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script></>
   );
 }
