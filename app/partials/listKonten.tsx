dc87e19aab331022e1b1b6fe6da74f18d38635d7
Rizky Daffy Al Fajr <162791897+RizkyDaffy@users.noreply.github.com>
2024-08-19 19:34:44 +0700

    Create listKonten.tsx

---
+++ app/partials/listKonten.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import '../index.js';
+
+function truncateText(text, maxSentences) {
+    const sentences = text.match(/[^.!?]+[.!?]+/g) || [text];
+    const truncated = sentences.slice(0, maxSentences).join(' ').trim();
+    return sentences.length > maxSentences ? truncated + '...' : truncated;
+}
+
+export default function ListKonten() {
+  const [tasks, setTasks] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    fetch('/api/task')
+      .then((response) => response.json())
+      .then((data) => {
+        if (Array.isArray(data)) {
+          setTasks(data);
+        } else {
+          console.error('Unexpected data format:', data);
+          setTasks([]);
+        }
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        setTasks([]);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <main>
+      {loading ? (
+        <div className="loading">
+          <p>sabar tod lagi baca database...</p>
+        </div>
+      ) : (
+        tasks.map((task, index) => (
+          <section
+            onClick={() => window.location.href = `/tugas/${task.Ids}`}
+            className="task"
+            key={index}
+          >
+            <h3 className="titekntn"><b>{task.Title}</b></h3>
+            <p>{truncateText(task.About, 1)}</p>
+            <div className="deadline">
+              <ion-icon name="time"></ion-icon>
+              <span> Deadline: {new Date(task.Deadline).toLocaleString()}</span>
+            </div>
+          </section>
+        ))
+      )}
+    </main>
+  );
+}
