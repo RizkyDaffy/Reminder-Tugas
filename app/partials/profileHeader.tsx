8b71e12cd46383a8e182330510213a258bb64066
Rizky Daffy Al Fajr <162791897+RizkyDaffy@users.noreply.github.com>
2024-08-19 20:01:40 +0700

    Update profileHeader.tsx

--- app/partials/profileHeader.tsx
+++ app/partials/profileHeader.tsx
@@ -8,6 +8,7 @@ export default function profileHeader() {
             <ion-icon name="person-circle" class="profile-icon"></ion-icon>
             <h2>Rizky Ganteng</h2>
         </div>
-    </header></>
+    </header>
+      <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script><script noModule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script></>
   );
 }
