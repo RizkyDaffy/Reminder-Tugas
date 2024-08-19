633d6128d46169292505d915aab90cb8b47cd32a
Rizky Daffy Al Fajr <162791897+RizkyDaffy@users.noreply.github.com>
2024-08-19 19:35:38 +0700

    Create profileKonten.tsx

---
+++ app/partials/profileKonten.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+export default function profileTop() {
+  return (
+    <><main className="p-main">
+        <section className="profile-task">
+            <h3>TUGAS BAHASA</h3>
+            <p>harus menjelaskan materi tentang ..</p>
+            <div className="profile-deadline">
+                <ion-icon name="time"></ion-icon>
+                <span>Deadline: Hari ini pukul 8:15</span>
+            </div>
+        </section>
+        <section className="profile-task">
+            <h3>TUGAS BAHASA</h3>
+            <p>harus menjelaskan materi tentang ..</p>
+            <div className="profile-deadline">
+                <ion-icon name="time"></ion-icon>
+                <span>Deadline: Hari ini pukul 8:15</span>
+            </div>
+        </section>
+    </main></>
+  );
+}
