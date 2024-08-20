78557fd92b67e8404f53718db76e0faa8202e6a9
Rizky Daffy Al Fajr <162791897+RizkyDaffy@users.noreply.github.com>
2024-08-20 17:38:54 +0700

    Update profileHeader.tsx

--- app/partials/profileHeader.tsx
+++ app/partials/profileHeader.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
+import { IonIcon } from '@ionic/react';
 
 export default function profileHeader() {
   return (
     <><header>
         <div className="profile">
-            <ion-icon name="person-circle" class="profile-icon"></ion-icon>
+            <IonIcon name="person-circle" class="profile-icon"></IonIcon>
             <h2>Rizky Ganteng</h2>
         </div>
-    </header>
-      <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script><script noModule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script></>
+    </header></>
   );
 }
