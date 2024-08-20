dcaa47bfb1a7eb6037db81925fc1e3a918e96859
Rizky Daffy Al Fajr <162791897+RizkyDaffy@users.noreply.github.com>
2024-08-20 18:30:53 +0700

    Update route.ts

--- app/api/task/route.ts
+++ app/api/task/route.ts
@@ -1,17 +1,22 @@
 // app/api/task/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import clientPromise from '../../../lib/mongodb';
-import { ObjectId } from 'mongodb'; // Import ObjectId
+import { ObjectId } from 'mongodb';
 
 export async function GET(req: NextRequest) {
   try {
     const client = await clientPromise;
-    const db = client.db('main-db'); // Ganti 'main-db' dengan nama database kamu
+    const db = client.db('main-db');
 
-    // Query ke MongoDB menggunakan ObjectId
+    // Log untuk debugging
+    console.log('Connected to MongoDB');
+
+    // Query MongoDB dengan ObjectId
     const result = await db.collection('all-db').findOne({
       _id: new ObjectId("66b43a652f119c6210cb115e")
     });
+
+    console.log('Result from MongoDB:', result);
 
     if (result && result.all) {
       return NextResponse.json(result.all);
@@ -19,7 +24,7 @@ export async function GET(req: NextRequest) {
       return NextResponse.json({ message: 'Data not found' }, { status: 404 });
     }
   } catch (e) {
-    console.error(e);
+    console.error('Error fetching data:', e);
     return NextResponse.json({ message: 'Failed to fetch tasks' }, { status: 500 });
   }
 }
