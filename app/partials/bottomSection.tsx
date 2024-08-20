13b93686d5aab3a502f1b74fbb781032e9298630
Rizky Daffy Al Fajr <162791897+RizkyDaffy@users.noreply.github.com>
2024-08-20 17:30:31 +0700

    Update bottomSection.tsx

--- app/partials/bottomSection.tsx
+++ app/partials/bottomSection.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
 import { IonIcon } from '@ionic/react';
-import { timeOutline } from 'ionicons/icons';
 
 export default function bottomSection() {
   return (
      <> <footer>
           <Link className="linknavg" href="/"> <div className="nav-icon">
-              <ion-icon name="home"></ion-icon>
+                  <IonIcon icon="home"></IonIcon>
            </div></Link>
            <Link className="linknavg nav-icon" href="/buat">
-               <ion-icon name="create"></ion-icon>
+                    <IonIcon icon="create"></IonIcon>
            </Link>
        </footer>
    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
