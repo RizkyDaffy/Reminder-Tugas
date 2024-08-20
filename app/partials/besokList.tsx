9d96ab20393039b2f9b1684bd1cf2da6d8dba9f8
Rizky Daffy Al Fajr <162791897+RizkyDaffy@users.noreply.github.com>
2024-08-20 17:22:46 +0700

    Update besokList.tsx

--- app/partials/besokList.tsx
+++ app/partials/besokList.tsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { IonIcon } from '@ionic/react';
+import { timeOutline } from 'ionicons/icons';  // Import the specific icon you want to use
 
-
-export default function besokList() {
+export default function BesokList() {
   return (
-    <><div id="besok" className="tabcontent">
+    <>
+      <div id="besok" className="tabcontent">
         <main>
-            <div className="task">
-                <h3 >TUGAS BAHASA</h3>
-                <p>harus menjelaskan materi tentang ..</p>
-                <div className="deadline">
-                    <ion-icon name="time"></ion-icon>
-                    <span>  Deadline: Besok pukul 8:15</span>
-                </div>
+          <div className="task">
+            <h3>TUGAS BAHASA</h3>
+            <p>harus menjelaskan materi tentang ..</p>
+            <div className="deadline">
+              <IonIcon icon={timeOutline} />
+              <span> Deadline: Besok pukul 8:15</span>
             </div>
+          </div>
         </main>
-    </div>
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script><script noModule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script></>
+      </div>
+      <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+      <script noModule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    </>
   );
 }
