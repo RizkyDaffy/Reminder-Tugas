acb3a6648e05c8340ac08f8414399bd0b2be04c9
Rizky Daffy Al Fajr <162791897+RizkyDaffy@users.noreply.github.com>
2024-08-20 17:39:38 +0700

    Update profileTop.tsx

--- app/partials/profileTop.tsx
+++ app/partials/profileTop.tsx
@@ -3,24 +3,11 @@ import React from 'react'
 
 export default function profileTop() {
   return (
-    <><main className="p-main">
-        <section className="profile-task">
-            <h3>TUGAS BAHASA</h3>
-            <p>harus menjelaskan materi tentang ..</p>
-            <div className="profile-deadline">
-                <ion-icon name="time"></ion-icon>
-                <span>Deadline: Hari ini pukul 8:15</span>
-            </div>
-        </section>
-        <section className="profile-task">
-            <h3>TUGAS BAHASA</h3>
-            <p>harus menjelaskan materi tentang ..</p>
-            <div className="profile-deadline">
-                <ion-icon name="time"></ion-icon>
-                <span>Deadline: Hari ini pukul 8:15</span>
-            </div>
-        </section>
-    </main>
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script><script noModule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script></>
+    <><nav className="profile-top-nav"><div className="profile-stats">
+            <a href="#" className="profile-active">0  selesai</a>
+            <a href="#">0  belum</a>
+            <a href="#">0  favorit</a>
+        </div>
+    </nav></>
   );
 }
