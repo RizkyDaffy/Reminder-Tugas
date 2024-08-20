fac039bf9b09da002ceab4cdf729c215aa563a14
Rizky Daffy Al Fajr <162791897+RizkyDaffy@users.noreply.github.com>
2024-08-20 18:23:16 +0700

    Update route.ts

--- app/api/task/route.ts
+++ app/api/task/route.ts
@@ -1,50 +1,16 @@
+// app/api/task/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import clientPromise from '../../../lib/mongodb';
-import { ObjectId } from 'mongodb';
-
-export async function POST(req: NextRequest) {
-  try {
-    const client = await clientPromise;
-    const db = client.db('main-db');
-
-    const data = await req.json();
-
-    // Ensure all fields are checked for existence before inserting
-    const newTask = {
-      Title: data.Title || "", // Default to empty string if undefined
-      About: data.About || "",
-      Deadline: data.Deadline || "",
-      Ids: data.Ids || "",
-      Guru: data.Guru || "",
-      Mapel: data.Mapel || "",
-      Tipe: data.Tipe || "",
-      Detail: data.Detail || "",
-    };
-
-    // Update the document by pushing the new task into the 'all' array
-    const result = await db.collection('all-db').updateOne(
-      { _id: new ObjectId("66b43a652f119c6210cb115e") }, // Find the specific document by its ObjectId
-      { $push: { all: newTask } } // Push the new task into the 'all' array
-    );
-
-    if (result.modifiedCount > 0) {
-      return NextResponse.json({ message: 'Task added successfully' }, { status: 201 });
-    } else {
-      return NextResponse.json({ message: 'Failed to add task' }, { status: 500 });
-    }
-  } catch (e) {
-    console.error(e);
-    return NextResponse.json({ message: 'Failed to create task' }, { status: 500 });
-  }
-}
+import { ObjectId } from 'mongodb'; // Import ObjectId
 
 export async function GET(req: NextRequest) {
   try {
     const client = await clientPromise;
-    const db = client.db('main-db');
+    const db = client.db('main-db'); // Ganti 'main-db' dengan nama database kamu
 
+    // Query ke MongoDB menggunakan ObjectId
     const result = await db.collection('all-db').findOne({
-      _id: new ObjectId("66b43a652f119c6210cb115e"),
+      _id: new ObjectId("66b43a652f119c6210cb115e")
     });
 
     if (result && result.all) {
