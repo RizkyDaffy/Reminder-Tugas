e20b3798aa7731a03afc00cff412b4a034393741
Rizky Daffy Al Fajr <162791897+RizkyDaffy@users.noreply.github.com>
2024-08-20 17:45:25 +0700

    Update tagModule.tsx

--- app/partials/tagModule.tsx
+++ app/partials/tagModule.tsx
@@ -5,9 +5,9 @@ export default function tagModule() {
   return (
     <> <div className="tab-container">
         <div className="tab">
-            <button className="tablinks center" onclick="openTab(event, 'besok')">Besok</button>
-            <button className="tablinks center" onclick="openTab(event, 'hari-ini')" id="defaultOpen">Hari ini</button>
-            <button className="tablinks center" onclick="openTab(event, 'info')">Info</button>
+            <button className="tablinks center" >Besok</button>
+            <button className="tablinks center">Hari ini</button>
+            <button className="tablinks center">Info</button>
         </div>
     </div>
     
